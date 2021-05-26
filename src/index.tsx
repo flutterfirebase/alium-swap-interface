0140fdb553496bd7417027b428cce62b0408b748
sieugene <sieugene@mail.ru>
2021-05-26 10:41:32 +0300

    add gtm

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
+import { ResetCSS } from '@alium-official/uikit'
+import 'inter-ui'
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import { ResetCSS } from '@alium-official/uikit'
-
-import GlobalStyle from './style/Global'
+import TagManager from 'react-gtm-module'
+import 'typeface-roboto'
+import Popups from './components/Popups'
+import ToastListener from './components/ToastListener'
+import './i18n'
 import App from './pages/App'
+import Providers from './Providers'
 import ApplicationUpdater from './state/application/updater'
 import ListsUpdater from './state/lists/updater'
 import MulticallUpdater from './state/multicall/updater'
 import TransactionUpdater from './state/transactions/updater'
-import ToastListener from './components/ToastListener'
-import Providers from './Providers'
-import 'inter-ui'
-import './i18n'
-import 'typeface-roboto'
-import Popups from './components/Popups'
+import GlobalStyle from './style/Global'
 
 if ('ethereum' in window) {
   (window.ethereum as any).autoRefreshOnNetworkChange = false
@@ -22,6 +22,12 @@ if ('ethereum' in window) {
 window.addEventListener('error', () => {
   localStorage?.removeItem('redux_localstorage_simple_lists')
 })
+
+const tagManagerArgs = {
+  gtmId: 'GTM-MWZ3WL5',
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(
   <StrictMode>
